3a0dbc57c083ba4b76835b1a03832d0685be9689
Wojciech Litewka <wojciech.litewka@jetbrains.com>
2023-10-30 10:53:48 +0100

    [BIR generator] Changes after rebase to master

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 import org.gradle.crypto.checksum.Checksum
 import org.gradle.plugins.ide.idea.model.IdeaModel
 import org.jetbrains.kotlin.gradle.targets.js.yarn.YarnLockMismatchReport
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
-import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
 import org.jetbrains.kotlin.utils.addToStdlib.ifTrue
 
 buildscript {
@@ -502,6 +500,7 @@ val projectsWithEnabledContextReceivers by extra {
         ":plugins:kapt4",
         ":compiler:bir",
         ":compiler:bir.tree",
+        ":compiler:bir.tree:tree-generator",
         ":compiler:bir.backend",
     )
 }
