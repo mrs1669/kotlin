aec912cea707afecc3e2268c3e67657f9944316d
Ivan Kylchik <ivan.kylchik@jetbrains.com>
2024-02-29 13:25:43 +0100

    [K2] Apply FIR constant evaluator to the `CONSTANT_EVALUATION` phase
    #KT-64151

--- analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/LLFirConstantEvaluationLazyResolver.kt
+++ analysis/low-level-api-fir/src/org/jetbrains/kotlin/analysis/low/level/api/fir/transformers/LLFirConstantEvaluationLazyResolver.kt
@@ -6,8 +6,13 @@
 package org.jetbrains.kotlin.analysis.low.level.api.fir.transformers
 
 import org.jetbrains.kotlin.analysis.low.level.api.fir.api.targets.LLFirResolveTarget
+import org.jetbrains.kotlin.analysis.low.level.api.fir.api.throwUnexpectedFirElementError
 import org.jetbrains.kotlin.fir.FirElementWithResolveState
-import org.jetbrains.kotlin.fir.declarations.FirResolvePhase
+import org.jetbrains.kotlin.fir.declarations.*
+import org.jetbrains.kotlin.fir.declarations.utils.isConst
+import org.jetbrains.kotlin.fir.resolve.ResolutionMode
+import org.jetbrains.kotlin.fir.resolve.transformers.FirCompileTimeConstantEvaluator
+import org.jetbrains.kotlin.fir.resolve.transformers.body.resolve.FirBodyResolveTransformer
 
 internal object LLFirConstantEvaluationLazyResolver : LLFirLazyResolver(FirResolvePhase.CONSTANT_EVALUATION) {
     override fun createTargetResolver(target: LLFirResolveTarget): LLFirTargetResolver = LLFirConstantEvaluationTargetResolver(target)
@@ -18,15 +23,39 @@ internal object LLFirConstantEvaluationLazyResolver : LLFirLazyResolver(FirResol
 /**
  * This resolver is responsible for [CONSTANT_EVALUATION][FirResolvePhase.CONSTANT_EVALUATION] phase.
  *
- * This resolver doesn't do anything yet (see KT-64151).
- *
  * @see FirResolvePhase.CONSTANT_EVALUATION
  */
-private class LLFirConstantEvaluationTargetResolver(resolveTarget: LLFirResolveTarget) : LLFirTargetResolver(
+private class LLFirConstantEvaluationTargetResolver(resolveTarget: LLFirResolveTarget) : LLFirAbstractBodyTargetResolver(
     resolveTarget,
     FirResolvePhase.CONSTANT_EVALUATION,
 ) {
+    override val transformer = object : FirBodyResolveTransformer(
+        session = resolveTargetSession,
+        phase = resolverPhase,
+        implicitTypeOnly = true,
+        scopeSession = resolveTargetScopeSession,
+    ) {
+        private val firCompileTimeConstantEvaluator = FirCompileTimeConstantEvaluator(session)
+
+        override fun transformProperty(property: FirProperty, data: ResolutionMode): FirProperty {
+            property.accept(firCompileTimeConstantEvaluator, null)
+            return property
+        }
+    }
+
     override fun doLazyResolveUnderLock(target: FirElementWithResolveState) {
-        // Will be finished later on
+        when (target) {
+            is FirProperty -> {
+                if (target.isConst) {
+                    resolve(target, BodyStateKeepers.PROPERTY)
+                }
+            }
+            is FirRegularClass, is FirTypeAlias, is FirFile, is FirCodeFragment, is FirAnonymousInitializer, is FirDanglingModifierList,
+            is FirEnumEntry, is FirErrorProperty, is FirScript, is FirFunction, is FirField
+            -> {
+                // No necessary to resolve these declarations
+            }
+            else -> throwUnexpectedFirElementError(target)
+        }
     }
 }
