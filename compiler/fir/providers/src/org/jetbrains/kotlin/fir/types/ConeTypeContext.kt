5a434895093a532f4e3db4d60bb364d80b931fe0
Mikhail Glukhikh <mikhail.glukhikh@jetbrains.com>
2024-03-27 15:19:25 +0100

    ConeTypeContext: consider stub constructor as a class(-like) constructor
    #KT-55383 Fixed

--- compiler/fir/providers/src/org/jetbrains/kotlin/fir/types/ConeTypeContext.kt
+++ compiler/fir/providers/src/org/jetbrains/kotlin/fir/types/ConeTypeContext.kt
@@ -310,7 +310,8 @@ interface ConeTypeContext : TypeSystemContext, TypeSystemOptimizationContext, Ty
     }
 
     override fun TypeConstructorMarker.isClassTypeConstructor(): Boolean {
-        return this is ConeClassLikeLookupTag
+        // See KT-55383
+        return this is ConeStubTypeConstructor || this is ConeClassLikeLookupTag
     }
 
     override fun TypeConstructorMarker.isInterface(): Boolean {
