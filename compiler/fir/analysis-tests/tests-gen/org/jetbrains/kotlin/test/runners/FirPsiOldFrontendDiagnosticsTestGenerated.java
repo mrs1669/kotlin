64bdd21f7cee23911372a105e87465164772aa24
Aleksandra.Arsenteva <aleksandra.arsenteva@jetbrains.com>
2024-03-28 16:52:08 +0100

    [Tests] Add tests for KT-58191

--- compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
+++ compiler/fir/analysis-tests/tests-gen/org/jetbrains/kotlin/test/runners/FirPsiOldFrontendDiagnosticsTestGenerated.java
@@ -34346,6 +34346,12 @@ public class FirPsiOldFrontendDiagnosticsTestGenerated extends AbstractFirPsiDia
         }
 
         @Test
+        @TestMetadata("lambdaWithCallInPlaceAndCycles.kt")
+        public void testLambdaWithCallInPlaceAndCycles() {
+          runTest("compiler/testData/diagnostics/tests/smartCasts/lambdasWithContracts/lambdaWithCallInPlaceAndCycles.kt");
+        }
+
+        @Test
         @TestMetadata("lambdaWithCallInPlaceAndDelegate.kt")
         public void testLambdaWithCallInPlaceAndDelegate() {
           runTest("compiler/testData/diagnostics/tests/smartCasts/lambdasWithContracts/lambdaWithCallInPlaceAndDelegate.kt");
