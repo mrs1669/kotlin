ebd2b06354aa67f230dd3ed685cd1a5ef29ec52f
Vyacheslav Gerasimov <vyacheslav.gerasimov@jetbrains.com>
2024-03-28 10:53:08 +0100

    ~~ -XX:CICompilerCount=4

--- repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
+++ repo/gradle-build-conventions/buildsrc-compat/src/main/kotlin/tasks.kt
@@ -216,6 +216,7 @@ fun Project.projectTest(
             "-XX:+UseCodeCacheFlushing",
             "-XX:ReservedCodeCacheSize=${reservedCodeCacheSizeMb}m",
             "-XX:MaxMetaspaceSize=${maxMetaspaceSizeMb}m",
+            "-XX:CICompilerCount=4",
             "-Djna.nosys=true"
         )
 
