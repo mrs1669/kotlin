2194b431ec0336711839b171a38259f106d149e5
Nikolay Lunyak <nikolay.lunyak@jetbrains.com>
2024-04-02 15:07:54 +0300

    fixup! [FIR] Add additional hints about `candidate.isSuccess`

--- compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/tower/CandidateApplicability.kt
+++ compiler/resolution.common/src/org/jetbrains/kotlin/resolve/calls/tower/CandidateApplicability.kt
@@ -132,10 +132,11 @@ enum class CandidateApplicability {
 annotation class ApplicabilityDetail
 
 /**
- * This property determines that the considered candidate is "successful" in terms of having no resolve errors.
- * Note that it does not necessarily mean tower resolve should stop on this candidate.
- * Neither does it mean that the candidate itself is successful because there is
- * [CandidateApplicability.RESOLVED_WITH_ERROR].
+ * Determines if this individual applicability is successful.
+ * Note that it does not necessarily mean tower resolve should stop on this candidate,
+ * and neither the value of `true` is enough to consider a candidate successful
+ * because there is [CandidateApplicability.RESOLVED_WITH_ERROR].
+ * Consider using [org.jetbrains.kotlin.fir.resolve.calls.Candidate.isSuccessful] if possible.
  */
 @ApplicabilityDetail
 val CandidateApplicability.isSuccess: Boolean
